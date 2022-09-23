7616ccd43479e3836058dcc624152829d70cf570
jainpranav1 <pranavji@tamu.edu>
2022-09-23 01:00:49 -0500

    fixed selector

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { chromium } = require("playwright");
 
-let player_id = "pranavjain001";
+let player_id = "GMshah";
 
 (async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto(`https://www.chess.com/member/${player_id}`);
-    let rating = await page.innerText("#view-profile > div.v5-section.v5-space-bottom.profile-header-container > div > div.profile-header-info > div.profile-header-details > div > div.profile-card-info > div.profile-card-user-info > div:nth-child(3) > div");
+    let rating = await page.innerText("body > div.base-layout > div.base-container > main > div.layout-column-two > div:nth-child(2) > div.stat-section-expanded.stat-section-stats-section > button > div > div.stat-section-user-rating > span");
     console.log(rating);
     await browser.close();
 })();
